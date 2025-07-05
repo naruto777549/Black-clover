b49e4d1b811f0bf0d7d02a76293da2e4f8a19834
naruto777549 <shekhikrar026@gmail.com>
2025-07-06 00:14:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-MONGO_URL = os.getenv("MONGO_URL", "your_mongo_url")
-ADMINS = [int(x) for x in os.getenv("ADMINS", "123456789").split()]
+API_ID = 123456  # Your actual API ID
+API_HASH = "your_api_hash_here"
+BOT_TOKEN = "your_bot_token_here"
+MONGO_URL = "your_mongodb_connection_url"
+ADMINS = [123456789]  # Your Telegram user ID (as list)
