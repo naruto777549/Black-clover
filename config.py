744450b0d60da9790c2715586e2a42e278a99546
naruto777549 <shekhikrar026@gmail.com>
2025-07-06 00:04:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "123456"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+MONGO_URL = os.getenv("MONGO_URL", "your_mongo_url")
+ADMINS = [int(x) for x in os.getenv("ADMINS", "123456789").split()]
