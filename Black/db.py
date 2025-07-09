65013c87b11142d1b8ec63008b5d7af9c2d0b4f9
naruto777549 <shekhikrar026@gmail.com>
2025-07-10 04:34:24 +0530

    Update db.py

--- Black/db.py
+++ Black/db.py
@@ -4,28 +4,4 @@ from config import MONGO_URL
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["BlackGameBot"]
 users = db["users"]
-
-# Example document structure:
-# {
-#   "_id": 12345678,
-#   "character": "Asta",
-#   "data": { ... },  # Asta's full character data from character.py
-#   "collection": [...]
-# }
-    {
-      "Name": "Asta",
-      "Attribute": "Anti-Magic",
-      "Rarity": "Legendary"
-    },
-    {
-      "Name": "Magna",
-      "Attribute": "Fire",
-      "Rarity": "Rare"
-    },
-    {
-      "Name": "Luck",
-      "Attribute": "Lightning",
-      "Rarity": "Epic"
-    }
-  ]
-}
+user["collection"]
