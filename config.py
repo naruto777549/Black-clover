2fa21b634177566bbd2adb245334f92efd017048
naruto777549 <shekhikrar026@gmail.com>
2025-07-10 04:12:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "12345"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-MONGO_URL = os.getenv("MONGO_URL", "your_mongo_url")
-ADMINS = list(map(int, os.getenv("ADMINS", "123456789").split()))
-BOT_USER = os.getenv("BOT_USER", "your_bot_username")
+API_ID = int(os.getenv("API_ID", "21218274"))
+API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8108826304:AAEIq1fvM39BJt_1tw6V0jitjAyoASeEkow")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://sufyan532011:5042@auctionbot.5ms20.mongodb.net/?retryWrites=true&w=majority&appName=AuctionBot")
+ADMINS = list(map(int, os.getenv("ADMINS", "7576729648").split()))
+BOT_USER = os.getenv("BOT_USER", "Shrutimusirobot")
