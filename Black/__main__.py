071ecddc6c6b8326a8e1e1140822bc2e722c7743
naruto777549 <shekhikrar026@gmail.com>
2025-07-10 15:25:15 +0530

    Update __main__.py

--- Black/__main__.py
+++ Black/__main__.py
@@ -2,7 +2,7 @@ from Black import bot
 from pyrogram import idle
 
 # Import all command handlers
-from Black.Modules import start, reset, char, shop, inventory, profile, Naruto
+from Black.Modules import start, reset, char, shop, inventory, profile, Naruto, add
 
 print("🚀 Starting the bot...")
 
